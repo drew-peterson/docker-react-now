38462c2fb3d8ce1fcce664af12d41ac2e69b699f
weeznog <drew.m.peterson@gmail.com>
2018-09-11 10:56:36 -0700

    update travis to full

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello drew</h1> <p>subheader here</p>
+        <h1>hello drew</h1>
+        <p>subheader here</p>
+        <p>sub here 2 here</p>
       </div>
     );
   }
