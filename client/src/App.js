1dcd2364d1270baa0f80600ef34f859c48527a21
weeznog <drew.m.peterson@gmail.com>
2018-09-11 15:12:52 -0700

    add local graphcool and apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
 
+const hello = gql`
+  {
+    hello(name: "drew") {
+      message
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: " http://localhost:60000/simple/v1/cjly9erov000b01008ptsy7tk"
+});
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello drew</h1>
-        <h2>drew is the best</h2>
-      </div>
+      <ApolloProvider client={client}>
+        <Query query={hello}>
+          {({ loading, error, data: { hello } }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error: ${error.message}`;
+
+            return <h1>{hello.message}</h1>;
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
