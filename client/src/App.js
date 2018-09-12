69fb712f8271d675b22de40961689a969c8dc14d
weeznog <drew.m.peterson@gmail.com>
2018-09-12 10:58:13 -0700

    change button text

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
               <div>
                 <h1>{hello.message}</h1>
                 <h4>sub header</h4>
-                <button onclick={refetch}>click me</button>
+                <button onclick={refetch}>refetch</button>
               </div>
             );
           }}
