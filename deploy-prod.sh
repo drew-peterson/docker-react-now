5135bc4c7c11bcbf8f980fe7b676c06d6be4b92d
weeznog <drew.m.peterson@gmail.com>
2018-09-13 16:35:52 -0700

    h3

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,7 +4,9 @@ now alias -A ./client/now.json --token $NOW_TOKEN
 now ./api --docker --public --token $NOW_TOKEN
 now alias -A ./api/now.json --token $NOW_TOKEN
 now alias drew-prod.now.sh -r rules.json --token $NOW_TOKEN
-now rm drew-client --safe --yes --token $NOW_TOKEN
-now rm drew-api --safe --yes --token $NOW_TOKEN
+# dont need to really rm since they will be frozen and removed automatically
+# also causes errors when first push
+# now rm drew-client --safe --yes --token $NOW_TOKEN
+# now rm drew-api --safe --yes --token $NOW_TOKEN
 # this worked -- though you might have to alias one manually first
 # or comment out previous travis above
--- deploy-stage.sh
+++ deploy-stage.sh
@@ -4,7 +4,7 @@ now alias -A ./client/now.stage.json --token $NOW_TOKEN
 now ./api --docker --public --token $NOW_TOKEN
 now alias -A ./api/now.stage.json --token $NOW_TOKEN
 now alias drew-stage.now.sh -r rules.stage.json --token $NOW_TOKEN
-now rm drew-client --safe --yes --token $NOW_TOKEN
-now rm drew-api --safe --yes --token $NOW_TOKEN
+# now rm drew-client --safe --yes --token $NOW_TOKEN
+# now rm drew-api --safe --yes --token $NOW_TOKEN
 # this worked -- though you might have to alias one manually first
 # or comment out previous travis above
