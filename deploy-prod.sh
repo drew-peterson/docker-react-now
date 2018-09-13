f931f4e1ebb78c9470c3717887f36e1883dcb2d9
weeznog <drew.m.peterson@gmail.com>
2018-09-12 17:46:36 -0700

    now 3

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,9 @@
-now ./client --docker --public --token $NOW_TOKEN
-now alias -A ./client/now.json --token $NOW_TOKEN
+# now ./client --docker --public --token $NOW_TOKEN
+# now alias -A ./client/now.json --token $NOW_TOKEN
 # now ./api --docker --public --token $NOW_TOKEN &&
 # now alias  --token $NOW_TOKEN &&
 # now alias drew-prod.now.sh -r rules.json --token $NOW_TOKEN
     
+cd client
+now --docker --public --token $NOW_TOKEN
+now alias  --token $NOW_TOKEN
