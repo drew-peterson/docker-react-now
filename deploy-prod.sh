4d7f5b538c71d60843dcfdddeb15d02c59f6ebb4
weeznog <drew.m.peterson@gmail.com>
2018-09-13 09:54:38 -0700

    now 7

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,9 +1,7 @@
-
-
-# this worked -- though you might have to alias one manually first
 now ./client --docker --public --token $NOW_TOKEN
 now alias -A ./client/now.json --token $NOW_TOKEN
 now ./api --docker --public --token $NOW_TOKEN
 now alias -A ./api/now.json --token $NOW_TOKEN
 now alias drew-prod.now.sh -r rules.json --token $NOW_TOKEN
+# this worked -- though you might have to alias one manually first
 # now rm docker-react-now --safe --yes --token $NOW_TOKEN
