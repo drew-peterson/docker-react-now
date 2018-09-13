8e168901f9983681869253bcdff807a050ebb569
weeznog <drew.m.peterson@gmail.com>
2018-09-13 10:07:48 -0700

    add stage rules

---
+++ deploy-stage.sh
@@ -0,0 +1,8 @@
+now ./client --docker --public --token $NOW_TOKEN
+now alias -A ./client/now.stage.json --token $NOW_TOKEN
+now ./api --docker --public --token $NOW_TOKEN
+now alias -A ./api/now.stage.json --token $NOW_TOKEN
+now alias drew-stage.now.sh -r rules.stage.json --token $NOW_TOKEN
+# this worked -- though you might have to alias one manually first
+# or comment out previous travis above
+# now rm docker-react-now --safe --yes --token $NOW_TOKEN
