cd0f34cc801cbe33411e1c206b6eb2374a90ef5a
weeznog <drew.m.peterson@gmail.com>
2018-09-13 09:42:52 -0700

    now 7

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,6 +5,6 @@
 # now alias drew-prod.now.sh -r rules.json --token $NOW_TOKEN
     
 now ./client --docker --public --token $NOW_TOKEN
-echo 'ALIAS works'
 now alias -A ./client/now.json --token $NOW_TOKEN
-echo $(now deploy)
+now ./api --docker --public --token $NOW_TOKEN
+now alias -A ./api/now.json --token $NOW_TOKEN
