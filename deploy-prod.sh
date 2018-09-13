8116563023a0faf6fb0dbdb62508ac70906ce0a7
weeznog <drew.m.peterson@gmail.com>
2018-09-12 17:43:37 -0700

    add deploy script

---
+++ deploy-prod.sh
@@ -0,0 +1,6 @@
+now ./client --docker --public --token $NOW_TOKEN
+now alias -A ./client/now.json --token $NOW_TOKEN
+# now ./api --docker --public --token $NOW_TOKEN &&
+# now alias  --token $NOW_TOKEN &&
+# now alias drew-prod.now.sh -r rules.json --token $NOW_TOKEN
+    
