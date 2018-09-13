2e1302ea84efdf9e8440403d4bdc03f5b15228f5
weeznog <drew.m.peterson@gmail.com>
2018-09-13 16:52:05 -0700

    cloud v2

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 export default () => (
   <div>
     <h1>Details</h1>
-    <h2>sub details here 3</h2>
+    <h2>sub details here 2</h2>
+    <h3>sub details here 3</h3>
     <Link to="/">Home</Link>
   </div>
 );
